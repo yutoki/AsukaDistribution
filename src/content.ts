f5d7b88232952f7aa23ee8846182106039859039
yutoki <ea11rltd@yahoo.co.jp>
2023-01-05 09:24:59 +0900

    遠距離系の特技使用率を追加

--- src/content.ts
+++ src/content.ts
@@ -8,16 +8,32 @@ interface IParameter {
     special: number
 }
 
+interface ILongMonster {
+    [Name: string]: ILongParameter
+}
+
+interface ILongParameter {
+    hp: number
+    attack: number
+    defence: number
+    longSpecial: number
+    special: number
+}
+
 window.addEventListener('load', init)
 
 function init() {
     var tbList = document.querySelectorAll('td')
     var monsterList: IMonster = require('./data/monster.json');
+    var longMonsterList: ILongMonster = require('./data/monsterLong.json')
 
     tbList.forEach(elem => {
         if (monsterList[elem.innerText] != undefined) {
-            var valMonster = monsterList[elem.innerText]
+            let valMonster = monsterList[elem.innerText]
             valueSet(elem, valMonster)
+        } else if (longMonsterList[elem.innerText] != undefined) {
+            let valMonster = longMonsterList[elem.innerText]
+            valueSetLong(elem, valMonster)
         }
     });
 }
@@ -30,6 +46,15 @@ function valueSet(tbCell: HTMLTableCellElement, monsterParameter: IParameter) {
     if (monsterParameter.special != 0) {
         tbCell.appendChild(setAttribute('data-category', 'Special', '特技率:' + monsterParameter.special))
     }
+}
+
+function valueSetLong(tbCell: HTMLTableCellElement, monsterParameter: ILongParameter) {
+    tbCell.appendChild(setAttribute('data-category', 'Rate', '出現率:' + 0))
+    tbCell.appendChild(setAttribute('data-category', 'Hp', 'HP:' + monsterParameter.hp))
+    tbCell.appendChild(setAttribute('data-category', 'Attack', 'Atk:' + monsterParameter.attack))
+    tbCell.appendChild(setAttribute('data-category', 'Defence', 'Def:' + monsterParameter.defence))
+    tbCell.appendChild(setAttribute('data-category', 'Special', '特技率(隣接時):' + monsterParameter.special))
+    tbCell.appendChild(setAttribute('data-category', 'Special', '特技率(非隣接時):' + monsterParameter.longSpecial))
 }
 
 function setAttribute(attrName: string, attrValue: string, textValue: string): HTMLDivElement {
